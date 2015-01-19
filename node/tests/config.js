41bf6a04ccff9d2f8b200ea91fc6968ec8d66e4b
Jess Portnoy <kernel01@gmail.com>
2015-01-19 21:55:55 +0200

    added admin secret and partner -2 secrets. added basic exception handling tests are now: session->start() partner->register()

--- node/tests/config.js
+++ node/tests/config.js
@@ -1,8 +1,20 @@
- var partnerId = 101;
+ var minus2_partner_id = -2;
+ var minus2_secret = 'a16c33ec08a992f49996a611ddcd24ac';
+ var minus2_admin_secret = 'a7feada5ebfaddc0494648b7f79ed3aa';
+ var partner_id = 101;
  var secret = '6aa758517fc892cf082185208a203116';
- var serviceUrl = 'http://54.159.220.35/' ;
- var userId = null;
- exports.partnerId = partnerId;
+ var admin_secret = '0567e52a030ebc2ccb73b386aa1800e0';
+ var service_url = 'http://54.159.220.35/' ;
+ var user_id = null;
+
+ exports.minus2_partner_id = minus2_partner_id;
+ exports.minus2_secret = minus2_secret;
+ exports.minus2_admin_secret = minus2_admin_secret;
+ exports.partner_id = partner_id;
  exports.secret = secret;
- exports.serviceUrl = serviceUrl;
- exports.userId = userId;
+ exports.admin_secret = admin_secret;
+ exports.partner_id = partner_id;
+ exports.secret = secret;
+ exports.admin_secret = admin_secret;
+ exports.service_url = service_url;
+ exports.user_id = user_id;

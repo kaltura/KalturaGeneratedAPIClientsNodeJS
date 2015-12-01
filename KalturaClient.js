375ff550e7e2b5e415f9fcc4f7bd7dff8c10c33d
Kaltura CSI <community@kaltura.com>
2015-12-01 01:11:40 -0500

    New clientlib ver

--- KalturaClient.js
+++ KalturaClient.js
@@ -37,7 +37,7 @@ kaltura.enums = require('./KalturaTypes');
 
 function KalturaClient(config) {
 	this.setApiVersion('3.3.0');
-	this.setClientTag('node:15-11-30');
+	this.setClientTag('node:15-12-01');
 	this.init(config);
 }
 

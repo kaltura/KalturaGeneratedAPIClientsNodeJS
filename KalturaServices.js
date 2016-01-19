1562b70e9e76bc469f696d39d828a4143063321a
Kaltura CSI <community@kaltura.com>
2016-01-19 09:28:41 -0500

    New clientlib ver

--- KalturaServices.js
+++ KalturaServices.js
@@ -7066,7 +7066,15 @@ KalturaUploadService.prototype.getUploadedFileTokenByFileName = function(callbac
  * The available service actions:
  * @action add Adds new upload token to upload a file.
  * @action get Get upload token by id.
- * @action upload Upload a file using the upload token id, returns an error on failure (an exception will be thrown when using one of the Kaltura clients).
+ * @action upload Upload a file using the upload token id, returns an error on failure (an exception will be thrown when using one of the Kaltura clients)
+ * Chunks can be uploaded in parallel and they will be appended according to their resumeAt position.
+ * A parallel upload session should have three stages:
+ * 1. A single upload with resume=false and finalChunk=false
+ * 2. Parallel upload requests each with resume=true,finalChunk=false and the expected resumetAt position.
+ * If a chunk fails to upload it can be re-uploaded.
+ * 3. After all of the chunks have been uploaded a final chunk (can be of zero size) should be uploaded
+ * with resume=true, finalChunk=true and the expected resumeAt position. In case an UPLOAD_TOKEN_CANNOT_MATCH_EXPECTED_SIZE exception
+ * has been returned (indicating not all of the chunks were appended yet) the final request can be retried.
  * @action delete Deletes the upload token by upload token id.
  * @action list List upload token by filter with pager support.
  * When using a user session the service will be restricted to users objects only.
@@ -7111,7 +7119,15 @@ KalturaUploadTokenService.prototype.get = function(callback, uploadTokenId){
 	}
 };
 /**
- * Upload a file using the upload token id, returns an error on failure (an exception will be thrown when using one of the Kaltura clients).
+ * Upload a file using the upload token id, returns an error on failure (an exception will be thrown when using one of the Kaltura clients)
+ * Chunks can be uploaded in parallel and they will be appended according to their resumeAt position.
+ * A parallel upload session should have three stages:
+ * 1. A single upload with resume=false and finalChunk=false
+ * 2. Parallel upload requests each with resume=true,finalChunk=false and the expected resumetAt position.
+ * If a chunk fails to upload it can be re-uploaded.
+ * 3. After all of the chunks have been uploaded a final chunk (can be of zero size) should be uploaded
+ * with resume=true, finalChunk=true and the expected resumeAt position. In case an UPLOAD_TOKEN_CANNOT_MATCH_EXPECTED_SIZE exception
+ * has been returned (indicating not all of the chunks were appended yet) the final request can be retried.
  * @param uploadTokenId string  (optional).
  * @param fileData file  (optional).
  * @param resume bool  (optional, default: false).

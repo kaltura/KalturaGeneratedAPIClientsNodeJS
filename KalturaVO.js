76f4ea161400e43fe1eefd6c37fab8d06ae10f81
Kaltura CSI <community@kaltura.com>
2016-09-23 13:07:44 -0400

    New clientlib ver

--- KalturaVO.js
+++ KalturaVO.js
@@ -13920,26 +13920,6 @@ util.inherits(KalturaFacebookDistributionProviderBaseFilter, KalturaDistribution
 
 /**
  */
-function KalturaFeedDropFolderBaseFilter(){
-	KalturaFeedDropFolderBaseFilter.super_.call(this);
-}
-module.exports.KalturaFeedDropFolderBaseFilter = KalturaFeedDropFolderBaseFilter;
-
-util.inherits(KalturaFeedDropFolderBaseFilter, KalturaDropFolderFilter);
-
-
-/**
- */
-function KalturaFeedDropFolderFileBaseFilter(){
-	KalturaFeedDropFolderFileBaseFilter.super_.call(this);
-}
-module.exports.KalturaFeedDropFolderFileBaseFilter = KalturaFeedDropFolderFileBaseFilter;
-
-util.inherits(KalturaFeedDropFolderFileBaseFilter, KalturaDropFolderFileFilter);
-
-
-/**
- */
 function KalturaFileAssetFilter(){
 	KalturaFileAssetFilter.super_.call(this);
 }
@@ -15145,26 +15125,6 @@ util.inherits(KalturaFacebookDistributionProviderFilter, KalturaFacebookDistribu
 
 
 /**
- */
-function KalturaFeedDropFolderFileFilter(){
-	KalturaFeedDropFolderFileFilter.super_.call(this);
-}
-module.exports.KalturaFeedDropFolderFileFilter = KalturaFeedDropFolderFileFilter;
-
-util.inherits(KalturaFeedDropFolderFileFilter, KalturaFeedDropFolderFileBaseFilter);
-
-
-/**
- */
-function KalturaFeedDropFolderFilter(){
-	KalturaFeedDropFolderFilter.super_.call(this);
-}
-module.exports.KalturaFeedDropFolderFilter = KalturaFeedDropFolderFilter;
-
-util.inherits(KalturaFeedDropFolderFilter, KalturaFeedDropFolderBaseFilter);
-
-
-/**
  * @param flavorParamsIdEqual int .
  * @param flavorParamsIdIn string .
  * @param statusEqual int .
@@ -15350,16 +15310,6 @@ function KalturaSyndicationDistributionProfileFilter(){
 module.exports.KalturaSyndicationDistributionProfileFilter = KalturaSyndicationDistributionProfileFilter;
 
 util.inherits(KalturaSyndicationDistributionProfileFilter, KalturaSyndicationDistributionProfileBaseFilter);
-
-
-/**
- */
-function KalturaSyndicationDistributionProviderFilter(){
-	KalturaSyndicationDistributionProviderFilter.super_.call(this);
-}
-module.exports.KalturaSyndicationDistributionProviderFilter = KalturaSyndicationDistributionProviderFilter;
-
-util.inherits(KalturaSyndicationDistributionProviderFilter, KalturaSyndicationDistributionProviderBaseFilter);
 
 
 /**

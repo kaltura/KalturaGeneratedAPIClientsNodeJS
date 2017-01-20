e33c78073bd00508ce293ca9c357f62c0802adbf
Kaltura CSI <community@kaltura.com>
2017-01-20 02:39:45 -0500

    New clientlib ver

--- KalturaModel.js
+++ KalturaModel.js
@@ -56991,6 +56991,30 @@ module.exports.FacebookDistributionProviderBaseFilter = FacebookDistributionProv
 /**
  *
  */
+class FeedDropFolderBaseFilter extends DropFolderFilter{
+	
+	constructor(object = null) {
+		super(object);
+		this.objectType = 'KalturaFeedDropFolderBaseFilter';
+	}
+}
+module.exports.FeedDropFolderBaseFilter = FeedDropFolderBaseFilter;
+
+/**
+ *
+ */
+class FeedDropFolderFileBaseFilter extends DropFolderFileFilter{
+	
+	constructor(object = null) {
+		super(object);
+		this.objectType = 'KalturaFeedDropFolderFileBaseFilter';
+	}
+}
+module.exports.FeedDropFolderFileBaseFilter = FeedDropFolderFileBaseFilter;
+
+/**
+ *
+ */
 class FileAssetFilter extends FileAssetBaseFilter{
 	
 	constructor(object = null) {
@@ -60177,6 +60201,30 @@ module.exports.FacebookDistributionProviderFilter = FacebookDistributionProvider
 /**
  *
  */
+class FeedDropFolderFileFilter extends FeedDropFolderFileBaseFilter{
+	
+	constructor(object = null) {
+		super(object);
+		this.objectType = 'KalturaFeedDropFolderFileFilter';
+	}
+}
+module.exports.FeedDropFolderFileFilter = FeedDropFolderFileFilter;
+
+/**
+ *
+ */
+class FeedDropFolderFilter extends FeedDropFolderBaseFilter{
+	
+	constructor(object = null) {
+		super(object);
+		this.objectType = 'KalturaFeedDropFolderFilter';
+	}
+}
+module.exports.FeedDropFolderFilter = FeedDropFolderFilter;
+
+/**
+ *
+ */
 class FlavorAssetBaseFilter extends AssetFilter{
 	
 	constructor(object = null) {
@@ -60512,6 +60560,18 @@ class SyndicationDistributionProfileFilter extends SyndicationDistributionProfil
 	}
 }
 module.exports.SyndicationDistributionProfileFilter = SyndicationDistributionProfileFilter;
+
+/**
+ *
+ */
+class SyndicationDistributionProviderFilter extends SyndicationDistributionProviderBaseFilter{
+	
+	constructor(object = null) {
+		super(object);
+		this.objectType = 'KalturaSyndicationDistributionProviderFilter';
+	}
+}
+module.exports.SyndicationDistributionProviderFilter = SyndicationDistributionProviderFilter;
 
 /**
  *

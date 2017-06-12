73c0be3fced0f09b911787b7fd8e6600bb598d06
Kaltura CSI <community@kaltura.com>
2017-06-12 01:55:10 -0400

    New clientlib ver

--- KalturaTypes.js
+++ KalturaTypes.js
@@ -3525,6 +3525,8 @@ module.exports.MetadataObjectType = {
 AD_CUE_POINT : 'adCuePointMetadata.AdCuePoint',
 ANNOTATION : 'annotationMetadata.Annotation',
 CODE_CUE_POINT : 'codeCuePointMetadata.CodeCuePoint',
+ANSWER_CUE_POINT : 'quiz.AnswerCuePoint',
+QUESTION_CUE_POINT : 'quiz.QuestionCuePoint',
 THUMB_CUE_POINT : 'thumbCuePointMetadata.thumbCuePoint',
 ENTRY : '1',
 CATEGORY : '2',

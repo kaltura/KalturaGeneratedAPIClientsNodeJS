7103cca372418f76074cbd283f2d5a258cd16fbd
Kaltura CSI <community@kaltura.com>
2019-01-07 01:27:34 -0500

    New clientlib ver

--- KalturaClient.js
+++ KalturaClient.js
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2018  Kaltura Inc.
+// Copyright (C) 2006-2019  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -43,7 +43,7 @@ class Client extends kaltura.ClientBase {
 	constructor(config) {
 		super(config);
 		this.setApiVersion('14.11.0');
-		this.setClientTag('node:19-01-06');
+		this.setClientTag('node:19-01-07');
 	}
 }
 

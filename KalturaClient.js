ab06ffb00f839bf43245c76715901b46a608f16a
Kaltura CSI <community@kaltura.com>
2019-03-05 01:13:57 -0500

    New clientlib ver

--- KalturaClient.js
+++ KalturaClient.js
@@ -43,7 +43,7 @@ class Client extends kaltura.ClientBase {
 	constructor(config) {
 		super(config);
 		this.setApiVersion('14.15.0');
-		this.setClientTag('node:19-03-04');
+		this.setClientTag('node:19-03-05');
 	}
 }
 

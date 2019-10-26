f9e6b9e9603b793948b57ff575c51b43b622786e
Kaltura CSI <community@kaltura.com>
2019-10-26 03:57:41 -0400

    New clientlib ver

--- KalturaTypes.js
+++ KalturaTypes.js
@@ -2801,6 +2801,7 @@ TAGS : 'tags',
 module.exports.ESearchEntryFieldName = {
 ACCESS_CONTROL_ID : 'access_control_id',
 ADMIN_TAGS : 'admin_tags',
+CAPTIONS_CONTENT : 'captions_content',
 CONVERSION_PROFILE_ID : 'conversion_profile_id',
 CREATED_AT : 'created_at',
 CREATOR_ID : 'creator_kuser_id',
